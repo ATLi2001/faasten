f8713721f428926a73a249ec066dca0660b7babe
Austin Li <42742696+ATLi2001@users.noreply.github.com>
2022-11-09 20:33:21 -0500

    Move DBENV to db_server

--- snapfaas/src/labeled_fs/utils.rs
+++ snapfaas/src/labeled_fs/utils.rs
@@ -5,6 +5,11 @@ use crate::distributed_db::db_client::DbClient;
 
 const ROOT: &str = "/";
 
+// return the bytes associated with a new directory
+pub fn get_new_dir_bytes() -> Vec<u8>{
+    labeled_fs::Directory::new().to_vec()
+}
+
 /// Utility function to create function directory under the root directory
 pub fn create_root_function_dir(name: &str, db_client: &mut DbClient) {
     let mut cur_label = DCLabel::bottom();
