ec68352a5ec52de5241f3ab5affcdefd100247dc
ATLi2001 <42742696+ATLi2001@users.noreply.github.com>
2023-02-25 12:34:28 -0500

    new workload

--- hello/workload.py
+++ hello/workload.py
@@ -1,12 +1,28 @@
 def handle(req, syscalls):
     syscalls.fs_createdir("/myapp", syscalls.get_current_label())
     syscalls.fs_createdir("/myapp/test_dir", syscalls.get_current_label())
-    syscalls.fs_createfile("/myapp/test_dir/hello.txt", syscalls.get_current_label())
-    syscalls.fs_write("/myapp/test_dir/hello.txt", bytes("HELLO TEST", "utf-8"))
 
+    for i in range(100):
+        syscalls.fs_createfile("myapp/test_dir/file%d.txt" % i, syscalls.get_current_label())
+        for j in range(100):
+            syscalls.fs_write("/myapp/test_dir/file%d.txt" % i, bytes("j=%d" % j, "utf-8"))
+
+    for i in range(100):
+        resp_i = syscalls.fs_read("/myapp/test_dir/file%d.txt" % i).decode("utf-8")
+        if resp_i != "j=99":
+            return {"error": "Got response %s from %s" % (resp_i, "file%d.txt" % i)}
+    
     syscalls.fs_createfile("/externalize.txt", syscalls.get_current_label())
     syscalls.fs_write("/externalize.txt", bytes("EXTERNALIZE", "utf-8"))
 
-    test_file = syscalls.fs_read("/myapp/test_dir/hello.txt").decode("utf-8")
+    return {"success": "SUCCESS"}
 
-    return {"test_file": test_file}
+    # syscalls.fs_createfile("/myapp/test_dir/hello.txt", syscalls.get_current_label())
+    # syscalls.fs_write("/myapp/test_dir/hello.txt", bytes("HELLO TEST", "utf-8"))
+
+    # syscalls.fs_createfile("/externalize.txt", syscalls.get_current_label())
+    # syscalls.fs_write("/externalize.txt", bytes("EXTERNALIZE", "utf-8"))
+
+    # test_file = syscalls.fs_read("/myapp/test_dir/hello.txt").decode("utf-8")
+
+    # return {"test_file": test_file}
