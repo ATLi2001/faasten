b918f6726e172544c55fe5f65e28674472ef8e68
ATLi2001 <42742696+ATLi2001@users.noreply.github.com>
2023-02-25 12:37:23 -0500

    new workload

--- hello/workload.py
+++ hello/workload.py
@@ -2,14 +2,14 @@ def handle(req, syscalls):
     syscalls.fs_createdir("/myapp", syscalls.get_current_label())
     syscalls.fs_createdir("/myapp/test_dir", syscalls.get_current_label())
 
-    for i in range(100):
+    for i in range(10):
         syscalls.fs_createfile("myapp/test_dir/file%d.txt" % i, syscalls.get_current_label())
-        for j in range(100):
+        for j in range(10):
             syscalls.fs_write("/myapp/test_dir/file%d.txt" % i, bytes("j=%d" % j, "utf-8"))
 
-    for i in range(100):
+    for i in range(10):
         resp_i = syscalls.fs_read("/myapp/test_dir/file%d.txt" % i).decode("utf-8")
-        if resp_i != "j=99":
+        if resp_i != "j=9":
             return {"error": "Got response %s from %s" % (resp_i, "file%d.txt" % i)}
     
     syscalls.fs_createfile("/externalize.txt", syscalls.get_current_label())
