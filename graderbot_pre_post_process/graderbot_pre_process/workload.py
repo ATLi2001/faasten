6311a3d5a08b9527a806c3cf0c4544f368dc9b82
ATLi2001 <42742696+ATLi2001@users.noreply.github.com>
2023-03-02 16:58:53 -0500

    sfdb for graderbot preprocess

--- graderbot_pre_post_process/graderbot_pre_process/workload.py
+++ graderbot_pre_post_process/graderbot_pre_process/workload.py
@@ -16,19 +16,19 @@ def handle(req, syscall):
 
 # preprocess by setting up database for graderbot functions
 def app_handle(args, context, syscall):
-    with open("~/graderbot-functions/output/example_cos316_grader.tgz", "rb") as f:
-        grading_script = f.read()
-    syscall.write_key(
-        bytes("cos316/example/grading_script", "utf-8"), 
-        grading_script
-    )
+    # with open("~/graderbot-functions/output/example_cos316_grader.tgz", "rb") as f:
+    #     grading_script = f.read()
+    # syscall.write_key(
+    #     bytes("cos316/example/grading_script", "utf-8"), 
+    #     grading_script
+    # )
 
-    with open("~/graderbot-functions/output/example_cos316_submission.tgz", "rb") as f:
-        submission = f.read()
-    syscall.write_key(
-        bytes("github/cos316/example/submission.tgz", "utf-8"), 
-        submission
-    )
+    # with open("~/graderbot-functions/output/example_cos316_submission.tgz", "rb") as f:
+    #     submission = f.read()
+    # syscall.write_key(
+    #     bytes("github/cos316/example/submission.tgz", "utf-8"), 
+    #     submission
+    # )
 
     assignments = {"example": {"grading_script": "cos316/example/grading_script", "runtime_limit": 1}}
     syscall.write_key(
