84eae7439356c9d4db9984206e80ab579165b683
Austin Li <austintianli@gmail.com>
2023-03-08 18:35:14 -0500

    synthetic bug fixes

--- synthetic/run_single_synthetic.sh
+++ synthetic/run_single_synthetic.sh
@@ -16,8 +16,12 @@ cd $ROOTDIR
 sudo RUST_LOG=debug $ROOTDIR/target/debug/multivm --config $ROOTDIR/synthetic/synthetic.yaml --mem 1024 --listen 127.0.0.1:3456 &
 PID=$!
 
+echo $PID
+# sleep so multivm can have time to start listen
+sleep 1
+
 # run sfclient
 sudo RUST_LOG=debug $ROOTDIR/target/debug/sfclient -s 127.0.0.1:3456 -f synthetic < $ROOTDIR/synthetic/synthetic_workload.json
 
 # send ^C to multivm
-kill -INT $PID
+sudo kill -INT $PID
