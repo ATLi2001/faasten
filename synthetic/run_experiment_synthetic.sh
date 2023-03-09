6f20100e9011ff807fd81705f45b23933c6af326
Austin Li <austintianli@gmail.com>
2023-03-09 17:45:02 -0500

    fix synthetic experiment bash scripts

--- synthetic/run_experiment_synthetic.sh
+++ synthetic/run_experiment_synthetic.sh
@@ -11,18 +11,25 @@ fi
 
 make -f Makefile
 
+# clear OUTDIR
+sudo rm -f $OUTDIR/*
+
+# for each time from MIN_MS to MAX_MS, incrementing by STEP each time
 t=$2
 while [ $t -le $3 ]; do
     echo $t 
     # run synthetic workload 
-    run_single_synthetic.sh $1 $t
+    bash run_single_synthetic.sh $1 $t
     # collect data
     for outfile in "$OUTDIR"/* 
-    do 
+    do
+	# only copy over non empty files
         if [ -s "$outfile" ]; then 
-            cp "$OUTDIR/$outfile" "$RESULTDIR/synthetic_${1}reps_${t}ms.json"
+            cp $outfile "$RESULTDIR/synthetic_${1}reps_${t}ms.json"
         fi
     done
 
+    sudo rm -f $OUTDIR/*
+    
     t=$(($t+$4))
 done
--- synthetic/run_single_synthetic.sh
+++ synthetic/run_single_synthetic.sh
@@ -14,9 +14,7 @@ cd $ROOTDIR
 
 # run multivm in background
 sudo RUST_LOG=debug $ROOTDIR/target/debug/multivm --config $ROOTDIR/synthetic/synthetic.yaml --mem 1024 --listen 127.0.0.1:3456 &
-PID=$!
 
-echo $PID
 # sleep so multivm can have time to start listen
 sleep 1
 
@@ -24,4 +22,4 @@ sleep 1
 sudo RUST_LOG=debug $ROOTDIR/target/debug/sfclient -s 127.0.0.1:3456 -f synthetic < $ROOTDIR/synthetic/synthetic_workload.json
 
 # send ^C to multivm
-sudo kill -INT $PID
+sudo kill -INT $(pidof multivm)
