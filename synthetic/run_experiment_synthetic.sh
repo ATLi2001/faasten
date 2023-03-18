5a4e45fd72d2e97b373a9db4973c0cf4e0cb4030
Austin Li <austintianli@gmail.com>
2023-03-18 10:44:23 -0400

    update run experiments

--- synthetic/run_experiment_synthetic.sh
+++ synthetic/run_experiment_synthetic.sh
@@ -4,8 +4,8 @@ ROOTDIR="/home/atli/faasten"
 OUTDIR="$ROOTDIR/out"
 RESULTDIR="$ROOTDIR/experiments/synthetic/single"
 
-if [ $# -ne 4 ]; then
-    echo 'usage: ./run_experiment_synthetic.sh REPS MIN_MS MAX_MS STEP'
+if [ $# -ne 5 ]; then
+    echo 'usage: ./run_experiment_synthetic.sh NAME REPS MIN_MS MAX_MS STEP'
     exit 1
 fi
 
@@ -15,21 +15,21 @@ make -f Makefile
 sudo rm -f $OUTDIR/*
 
 # for each time from MIN_MS to MAX_MS, incrementing by STEP each time
-t=$2
-while [ $t -le $3 ]; do
+t=$3
+while [ $t -le $4 ]; do
     echo $t 
     # run synthetic workload 
-    bash run_single_synthetic.sh $1 $t
+    bash run_single_synthetic.sh $2 $t
     # collect data
     for outfile in "$OUTDIR"/* 
     do
 	# only copy over non empty files
         if [ -s "$outfile" ]; then 
-            cp $outfile "$RESULTDIR/synthetic_${1}reps_${t}ms.json"
+            cp $outfile "$RESULTDIR/${1}/synthetic_${2}reps_${t}ms.json"
         fi
     done
 
     sudo rm -f $OUTDIR/*
     
-    t=$(($t+$4))
+    t=$(($t+$5))
 done
--- synthetic/run_single_synthetic.sh
+++ synthetic/run_single_synthetic.sh
@@ -13,13 +13,13 @@ echo "{\"args\": {\"reps\": $1, \"interop_compute_ms\": $2}, \"workflow\": [ ], 
 cd $ROOTDIR
 
 # run multivm in background
-sudo RUST_LOG=debug $ROOTDIR/target/debug/multivm --config $ROOTDIR/synthetic/synthetic.yaml --mem 1024 --listen 127.0.0.1:3456 &
+sudo $ROOTDIR/target/debug/multivm --config $ROOTDIR/synthetic/synthetic.yaml --mem 1024 --listen 127.0.0.1:3456 &
 
 # sleep so multivm can have time to start listen
 sleep 1
 
 # run sfclient
-sudo RUST_LOG=debug $ROOTDIR/target/debug/sfclient -s 127.0.0.1:3456 -f synthetic < $ROOTDIR/synthetic/synthetic_workload.json
+sudo $ROOTDIR/target/debug/sfclient -s 127.0.0.1:3456 -f synthetic < $ROOTDIR/synthetic/synthetic_workload.json
 
 # send ^C to multivm
 sudo kill -INT $(pidof multivm)
